1733a9fbdf52d32cdd127b1089af55cfd33205bb
ysrael28 <ysrael.m@escolar.ifrn.edu.br>
2026-06-30 14:58:59 -0300

    deu cerrtto

--- motores3pjd3v2026/Assets/scripts/moeda/Coin.cs
+++ motores3pjd3v2026/Assets/scripts/moeda/Coin.cs
@@ -4,13 +4,22 @@ public class Coin : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-      
         if (other.CompareTag("Player"))
         {
-           
-            
-
        
+            PlayerController player = other.GetComponent<PlayerController>();
+            
+           
+            if (player != null)
+            {
+                player.AddCoin();
+            }
+            else
+            {
+                Debug.LogWarning("<color=red>[Coin]</color> Colidi com o Player, mas não achei o script de controle nele! Verifique o nome do script.");
+            }
+
+           
             Destroy(gameObject);
         }
     }
